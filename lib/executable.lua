936f29cd06d13370ccb0a02f762da967eb81693a
Gavin Kistner <gkistner@nvidia.com>
2013-02-12 12:33:26 -0700

    Add <send> minimal support; pass 4/10

--- lib/executable.lua
+++ lib/executable.lua
@@ -8,6 +8,20 @@ function LXSC.EXECUTABLE:raise(scxml)
 	scxml:fireEvent(self.event,nil,true)
 end
 
+function LXSC.EXECUTABLE:send(scxml)
+	-- TODO: warn about delay/delayexpr no support
+	-- TODO: support type/typeexpr/target/targetexpr 
+	local dm = scxml.datamodel
+	local name = self.event or dm:run(self.eventexpr)
+	local data
+	if self.namelist then
+		data = {}
+		for name in string.gmatch(self.namelist,'[^%s]+') do data[name] = dm:get(name) end
+	end
+	if self.idlocation and not self.id then dm:set( dm:run(self.idlocation), LXSC.uuid4() ) end
+	scxml:fireEvent(name,data,false)
+end
+
 function LXSC.SCXML:executeContent(item)
 	local handler = LXSC.EXECUTABLE[item.kind] 
 	if handler then
